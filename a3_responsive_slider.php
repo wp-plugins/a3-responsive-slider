b6a0538eefb4b0692d0c38dac830d311f5c4f17e
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-18 08:45:54 +0000

    Created tag 1.1.0.
    git-svn-id: https://plugins.svn.wordpress.org/a3-responsive-slider/tags/1.1.0@1047886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- a3_responsive_slider.php
+++ a3_responsive_slider.php
@@ -2,11 +2,11 @@
 /*
 Plugin Name: A3 Responsive Slider
 Description: Create unlimited robust and flexible responsive image sliders. Insert them by shortcode from the text editor on any post, custom post type or page or add widget. Auto Mobile touch swipe and a fully customizable skin.
-Version: 1.0.0.2
+Version: 1.1.0
 Author: A3 Revolution
 Author URI: http://www.a3rev.com/
 Requires at least: 3.8
-Tested up to: 3.9.1
+Tested up to: 4.0
 License: GPLv2 or later
 	Copyright © 2011 A3 Revolution Software Development team
 	A3 Revolution Software Development team
@@ -15,46 +15,45 @@ License: GPLv2 or later
 	Gympie 4570
 	QLD Australia
 */
-
 ?>
 <?php
-define( 'A3_RESPONSIVE_SLIDER_FILE_PATH', dirname( __FILE__ ) );
-define( 'A3_RESPONSIVE_SLIDER_DIR_NAME', basename( A3_RESPONSIVE_SLIDER_FILE_PATH ) );
-define( 'A3_RESPONSIVE_SLIDER_FOLDER', dirname( plugin_basename( __FILE__) ) );
-define( 'A3_RESPONSIVE_SLIDER_NAME', plugin_basename( __FILE__ ) );
-define( 'A3_RESPONSIVE_SLIDER_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
-define( 'A3_RESPONSIVE_SLIDER_DIR', WP_CONTENT_DIR.'/plugins/'.A3_RESPONSIVE_SLIDER_FOLDER);
-define( 'A3_RESPONSIVE_SLIDER_JS_URL',  A3_RESPONSIVE_SLIDER_URL . '/assets/js' );
-define( 'A3_RESPONSIVE_SLIDER_EXTENSION_JS_URL',  A3_RESPONSIVE_SLIDER_URL . '/assets/js/cycle2-extensions' );
-define( 'A3_RESPONSIVE_SLIDER_CSS_URL',  A3_RESPONSIVE_SLIDER_URL . '/assets/css' );
-define( 'A3_RESPONSIVE_SLIDER_IMAGES_URL',  A3_RESPONSIVE_SLIDER_URL . '/assets/images' );
-if(!defined("A3_RESPONSIVE_SLIDER_PRO_VERSION_URI"))
-    define("A3_RESPONSIVE_SLIDER_PRO_VERSION_URI", "http://a3rev.com/shop/a3-responsive-slider/");
+define('A3_RESPONSIVE_SLIDER_FILE_PATH', dirname(__FILE__));
+define('A3_RESPONSIVE_SLIDER_DIR_NAME', basename(A3_RESPONSIVE_SLIDER_FILE_PATH));
+define('A3_RESPONSIVE_SLIDER_FOLDER', dirname(plugin_basename(__FILE__)));
+define('A3_RESPONSIVE_SLIDER_NAME', plugin_basename(__FILE__));
+define('A3_RESPONSIVE_SLIDER_URL', untrailingslashit(plugins_url('/', __FILE__)));
+define('A3_RESPONSIVE_SLIDER_DIR', WP_CONTENT_DIR . '/plugins/' . A3_RESPONSIVE_SLIDER_FOLDER);
+define('A3_RESPONSIVE_SLIDER_JS_URL', A3_RESPONSIVE_SLIDER_URL . '/assets/js');
+define('A3_RESPONSIVE_SLIDER_EXTENSION_JS_URL', A3_RESPONSIVE_SLIDER_URL . '/assets/js/cycle2-extensions');
+define('A3_RESPONSIVE_SLIDER_CSS_URL', A3_RESPONSIVE_SLIDER_URL . '/assets/css');
+define('A3_RESPONSIVE_SLIDER_IMAGES_URL', A3_RESPONSIVE_SLIDER_URL . '/assets/images');
+if (!defined("A3_RESPONSIVE_SLIDER_PRO_VERSION_URI")) define("A3_RESPONSIVE_SLIDER_PRO_VERSION_URI", "http://a3rev.com/shop/a3-responsive-slider/");
 
-include( 'admin/admin-ui.php' );
-include( 'admin/admin-interface.php' );
+include ('admin/admin-ui.php');
+include ('admin/admin-interface.php');
 
-include( 'classes/a3-rslider-functions.php' );
-include( 'admin/classes/a3-rslider-edit.php' );
+include ('classes/a3-rslider-functions.php');
+include ('admin/classes/a3-rslider-edit.php');
 
-include('admin/admin-pages/admin-slider-skins-page.php');
-include('admin/admin-pages/admin-card-skin-page.php');
-include('admin/admin-pages/admin-templates_widget-page.php');
-include('admin/admin-pages/admin-templates_mobile-page.php');
+include ('admin/admin-pages/admin-slider-skins-page.php');
+include ('admin/admin-pages/admin-card-skin-page.php');
+include ('admin/admin-pages/admin-templates_widget-page.php');
+include ('admin/admin-pages/admin-templates_mobile-page.php');
 
-include( 'admin/admin-init.php' );
+include ('admin/admin-init.php');
+include ('admin/less/sass.php');
 
-include( 'classes/data/a3-rslider-data.php' );
-include( 'classes/a3-rslider-custom-post.php' );
-include( 'classes/a3-rslider-duplicate.php' );
-include( 'classes/a3-rslider-hook-filter.php' );
-include( 'classes/a3-rslider-display.php' );
-include( 'shortcodes/class-rslider-shortcodes.php' );
-include( 'widgets/class-rslider-widgets.php' );
+include ('classes/data/a3-rslider-data.php');
+include ('classes/a3-rslider-custom-post.php');
+include ('classes/a3-rslider-duplicate.php');
+include ('classes/a3-rslider-hook-filter.php');
+include ('classes/a3-rslider-display.php');
+include ('shortcodes/class-rslider-shortcodes.php');
+include ('widgets/class-rslider-widgets.php');
 
-include( 'preview/a3-rslider-preview.php' );
+include ('preview/a3-rslider-preview.php');
 
-include( 'admin/a3-rslider-admin.php' );
+include ('admin/a3-rslider-admin.php');
 
-register_activation_hook( __FILE__, 'a3_rslider_activated' );
+register_activation_hook(__FILE__, 'a3_rslider_activated');
 ?>
